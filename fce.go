ecb0531d7f22caa6da69b9b2ab398cfaed5caba9
skirit <skirit@dump.cz>
2021-07-26 00:22:06 +0200

    add display of temp, press and react to light

---
+++ fce.go
@@ -0,0 +1,197 @@
+package main
+
+import (
+		"strings"
+		"strconv"
+		"encoding/json"
+		"github.com/go-resty/resty/v2"
+)
+
+func GetPrice() int {
+	client := resty.New()
+
+	resp, err := client.R().
+		EnableTrace().
+		Get("https://api.bitfinex.com/v1/pubticker/btcusd")
+	if err == nil {
+		type Pars struct {
+			Price	string `json:"last_price"`
+		}
+		pars := Pars{}
+		json.Unmarshal(resp.Body(), &pars)
+		btc := strings.Split(pars.Price,".")
+		btci, _ := strconv.Atoi(btc[0])
+		return btci
+	}
+	return 0
+}
+
+func GetTemp() float32 {
+	client := resty.New()
+
+	resp, err := client.R().
+		EnableTrace().
+		Get("http://172.17.0.133:10000/data")
+	if err == nil {
+		type Pars struct {
+			Temp	float32 `json:"temperature"`
+		}
+		pars := Pars{}
+		json.Unmarshal(resp.Body(), &pars)
+		return pars.Temp
+	}
+	return 0
+}
+
+func GetPress() float32 {
+	client := resty.New()
+
+	resp, err := client.R().
+		EnableTrace().
+		Get("http://172.17.0.133:10000/data")
+	if err == nil {
+		type Pars struct {
+			Press	float32 `json:"pressure"`
+		}
+		pars := Pars{}
+		json.Unmarshal(resp.Body(), &pars)
+		return pars.Press
+	}
+	return 0
+}
+
+func GetIll() int {
+	client := resty.New()
+
+	resp, err := client.R().
+		EnableTrace().
+		Get("http://172.17.0.123:10000/data")
+	if err == nil {
+		type Pars struct {
+			Ill		int `json:"illuminance"`
+		}
+		pars := Pars{}
+		json.Unmarshal(resp.Body(), &pars)
+		return pars.Ill
+	}
+	return 0
+}
+
+func TextToRGB(fg uint32, bg uint32, text string, data []uint32){
+    for i := 0; i < len(text) && i < panels; i++ {
+        CharToRGB(fg, bg, text[i], (i*panel)+first, data)
+    }
+}
+
+func CharToRGB(fg uint32, bg uint32, c byte, i int, data []uint32){
+	switch c{
+		case '1':
+			data[i+0] = bg; data[i+15] = bg; data[i+16] = fg; data[i+31] = bg; data[i+32] = bg
+			data[i+1] = bg; data[i+14] = fg; data[i+17] = fg; data[i+30] = bg; data[i+33] = bg
+			data[i+2] = bg; data[i+13] = bg; data[i+18] = fg; data[i+29] = bg; data[i+34] = bg
+			data[i+3] = bg; data[i+12] = bg; data[i+19] = fg; data[i+28] = bg; data[i+35] = bg
+			data[i+4] = bg; data[i+11] = bg; data[i+20] = fg; data[i+27] = bg; data[i+36] = bg
+			data[i+5] = bg; data[i+10] = bg; data[i+21] = fg; data[i+26] = bg; data[i+37] = bg
+			data[i+6] = bg; data[i+9]  = bg; data[i+22] = fg; data[i+25] = bg; data[i+38] = bg
+			data[i+7] = bg; data[i+8]  = fg; data[i+23] = fg; data[i+24] = fg; data[i+39] = bg
+		case '2':
+			data[i+0] = bg; data[i+15] = fg; data[i+16] = fg; data[i+31] = fg; data[i+32] = bg
+			data[i+1] = fg; data[i+14] = bg; data[i+17] = bg; data[i+30] = bg; data[i+33] = fg
+			data[i+2] = bg; data[i+13] = bg; data[i+18] = bg; data[i+29] = bg; data[i+34] = fg
+			data[i+3] = bg; data[i+12] = bg; data[i+19] = bg; data[i+28] = bg; data[i+35] = fg
+			data[i+4] = bg; data[i+11] = bg; data[i+20] = bg; data[i+27] = fg; data[i+36] = bg
+			data[i+5] = bg; data[i+10] = bg; data[i+21] = fg; data[i+26] = bg; data[i+37] = bg
+			data[i+6] = bg; data[i+9]  = fg; data[i+22] = bg; data[i+25] = bg; data[i+38] = bg
+			data[i+7] = fg; data[i+8]  = fg; data[i+23] = fg; data[i+24] = fg; data[i+39] = fg
+		case '3':
+			data[i+0] = fg; data[i+15] = fg; data[i+16] = fg; data[i+31] = fg; data[i+32] = fg
+			data[i+1] = bg; data[i+14] = bg; data[i+17] = bg; data[i+30] = bg; data[i+33] = fg
+			data[i+2] = bg; data[i+13] = bg; data[i+18] = bg; data[i+29] = fg; data[i+34] = bg
+			data[i+3] = bg; data[i+12] = bg; data[i+19] = fg; data[i+28] = bg; data[i+35] = bg
+			data[i+4] = bg; data[i+11] = bg; data[i+20] = bg; data[i+27] = fg; data[i+36] = bg
+			data[i+5] = bg; data[i+10] = bg; data[i+21] = bg; data[i+26] = bg; data[i+37] = fg
+			data[i+6] = fg; data[i+9]  = bg; data[i+22] = bg; data[i+25] = bg; data[i+38] = fg
+			data[i+7] = bg; data[i+8]  = fg; data[i+23] = fg; data[i+24] = fg; data[i+39] = bg
+		case '4':
+			data[i+0] = fg; data[i+15] = bg; data[i+16] = bg; data[i+31] = bg; data[i+32] = bg
+			data[i+1] = fg; data[i+14] = bg; data[i+17] = bg; data[i+30] = bg; data[i+33] = bg
+			data[i+2] = fg; data[i+13] = bg; data[i+18] = fg; data[i+29] = bg; data[i+34] = bg
+			data[i+3] = fg; data[i+12] = bg; data[i+19] = fg; data[i+28] = bg; data[i+35] = bg
+			data[i+4] = fg; data[i+11] = bg; data[i+20] = fg; data[i+27] = bg; data[i+36] = bg
+			data[i+5] = fg; data[i+10] = fg; data[i+21] = fg; data[i+26] = fg; data[i+37] = fg
+			data[i+6] = bg; data[i+9]  = bg; data[i+22] = fg; data[i+25] = bg; data[i+38] = bg
+			data[i+7] = bg; data[i+8]  = bg; data[i+23] = fg; data[i+24] = bg; data[i+39] = bg
+		case '5':
+			data[i+0] = fg; data[i+15] = fg; data[i+16] = fg; data[i+31] = fg; data[i+32] = fg
+			data[i+1] = fg; data[i+14] = bg; data[i+17] = bg; data[i+30] = bg; data[i+33] = bg
+			data[i+2] = fg; data[i+13] = bg; data[i+18] = bg; data[i+29] = bg; data[i+34] = bg
+			data[i+3] = fg; data[i+12] = fg; data[i+19] = fg; data[i+28] = fg; data[i+35] = bg
+			data[i+4] = bg; data[i+11] = bg; data[i+20] = bg; data[i+27] = bg; data[i+36] = fg
+			data[i+5] = bg; data[i+10] = bg; data[i+21] = bg; data[i+26] = bg; data[i+37] = fg
+			data[i+6] = fg; data[i+9]  = bg; data[i+22] = bg; data[i+25] = bg; data[i+38] = fg
+			data[i+7] = bg; data[i+8]  = fg; data[i+23] = fg; data[i+24] = fg; data[i+39] = bg
+		case '6':
+			data[i+0] = bg; data[i+15] = fg; data[i+16] = fg; data[i+31] = fg; data[i+32] = bg
+			data[i+1] = fg; data[i+14] = bg; data[i+17] = bg; data[i+30] = bg; data[i+33] = fg
+			data[i+2] = fg; data[i+13] = bg; data[i+18] = bg; data[i+29] = bg; data[i+34] = bg
+			data[i+3] = fg; data[i+12] = fg; data[i+19] = fg; data[i+28] = fg; data[i+35] = bg
+			data[i+4] = fg; data[i+11] = bg; data[i+20] = bg; data[i+27] = bg; data[i+36] = fg
+			data[i+5] = fg; data[i+10] = bg; data[i+21] = bg; data[i+26] = bg; data[i+37] = fg
+			data[i+6] = fg; data[i+9]  = bg; data[i+22] = bg; data[i+25] = bg; data[i+38] = fg
+			data[i+7] = bg; data[i+8]  = fg; data[i+23] = fg; data[i+24] = fg; data[i+39] = bg
+		case '7':
+			data[i+0] = fg; data[i+15] = fg; data[i+16] = fg; data[i+31] = fg; data[i+32] = fg
+			data[i+1] = bg; data[i+14] = bg; data[i+17] = bg; data[i+30] = bg; data[i+33] = fg
+			data[i+2] = bg; data[i+13] = bg; data[i+18] = bg; data[i+29] = fg; data[i+34] = bg
+			data[i+3] = bg; data[i+12] = bg; data[i+19] = fg; data[i+28] = bg; data[i+35] = bg
+			data[i+4] = bg; data[i+11] = bg; data[i+20] = fg; data[i+27] = bg; data[i+36] = bg
+			data[i+5] = bg; data[i+10] = fg; data[i+21] = bg; data[i+26] = bg; data[i+37] = bg
+			data[i+6] = bg; data[i+9]  = fg; data[i+22] = bg; data[i+25] = bg; data[i+38] = bg
+			data[i+7] = bg; data[i+8]  = fg; data[i+23] = bg; data[i+24] = bg; data[i+39] = bg
+		case '8':
+			data[i+0] = bg; data[i+15] = fg; data[i+16] = fg; data[i+31] = fg; data[i+32] = bg
+			data[i+1] = fg; data[i+14] = bg; data[i+17] = bg; data[i+30] = bg; data[i+33] = fg
+			data[i+2] = fg; data[i+13] = bg; data[i+18] = bg; data[i+29] = bg; data[i+34] = fg
+			data[i+3] = bg; data[i+12] = fg; data[i+19] = fg; data[i+28] = fg; data[i+35] = bg
+			data[i+4] = fg; data[i+11] = bg; data[i+20] = bg; data[i+27] = bg; data[i+36] = fg
+			data[i+5] = fg; data[i+10] = bg; data[i+21] = bg; data[i+26] = bg; data[i+37] = fg
+			data[i+6] = fg; data[i+9]  = bg; data[i+22] = bg; data[i+25] = bg; data[i+38] = fg
+			data[i+7] = bg; data[i+8]  = fg; data[i+23] = fg; data[i+24] = fg; data[i+39] = bg
+		case '9':
+			data[i+0] = bg; data[i+15] = fg; data[i+16] = fg; data[i+31] = fg; data[i+32] = bg
+			data[i+1] = fg; data[i+14] = bg; data[i+17] = bg; data[i+30] = bg; data[i+33] = fg
+			data[i+2] = fg; data[i+13] = bg; data[i+18] = bg; data[i+29] = bg; data[i+34] = fg
+			data[i+3] = fg; data[i+12] = bg; data[i+19] = bg; data[i+28] = bg; data[i+35] = fg
+			data[i+4] = bg; data[i+11] = fg; data[i+20] = fg; data[i+27] = fg; data[i+36] = fg
+			data[i+5] = bg; data[i+10] = bg; data[i+21] = bg; data[i+26] = bg; data[i+37] = fg
+			data[i+6] = fg; data[i+9]  = bg; data[i+22] = bg; data[i+25] = bg; data[i+38] = fg
+			data[i+7] = bg; data[i+8]  = fg; data[i+23] = fg; data[i+24] = fg; data[i+39] = bg
+		case '0':
+			data[i+0] = bg; data[i+15] = fg; data[i+16] = fg; data[i+31] = fg; data[i+32] = bg
+			data[i+1] = fg; data[i+14] = bg; data[i+17] = bg; data[i+30] = bg; data[i+33] = fg
+			data[i+2] = fg; data[i+13] = bg; data[i+18] = bg; data[i+29] = bg; data[i+34] = fg
+			data[i+3] = fg; data[i+12] = bg; data[i+19] = bg; data[i+28] = fg; data[i+35] = fg
+			data[i+4] = fg; data[i+11] = bg; data[i+20] = fg; data[i+27] = bg; data[i+36] = fg
+			data[i+5] = fg; data[i+10] = fg; data[i+21] = bg; data[i+26] = bg; data[i+37] = fg
+			data[i+6] = fg; data[i+9]  = bg; data[i+22] = bg; data[i+25] = bg; data[i+38] = fg
+			data[i+7] = bg; data[i+8]  = fg; data[i+23] = fg; data[i+24] = fg; data[i+39] = bg
+		case '.':
+			data[i+0] = bg; data[i+15] = bg; data[i+16] = bg; data[i+31] = bg; data[i+32] = bg
+			data[i+1] = bg; data[i+14] = bg; data[i+17] = bg; data[i+30] = bg; data[i+33] = bg
+			data[i+2] = bg; data[i+13] = bg; data[i+18] = bg; data[i+29] = bg; data[i+34] = bg
+			data[i+3] = bg; data[i+12] = bg; data[i+19] = bg; data[i+28] = bg; data[i+35] = bg
+			data[i+4] = bg; data[i+11] = bg; data[i+20] = bg; data[i+27] = bg; data[i+36] = bg
+			data[i+5] = bg; data[i+10] = bg; data[i+21] = bg; data[i+26] = bg; data[i+37] = bg
+			data[i+6] = bg; data[i+9]  = bg; data[i+22] = bg; data[i+25] = bg; data[i+38] = bg
+			data[i+7] = bg; data[i+8]  = bg; data[i+23] = fg; data[i+24] = bg; data[i+39] = bg
+		default :
+			data[i+0] = bg; data[i+15] = bg; data[i+16] = bg; data[i+31] = bg; data[i+32] = bg
+			data[i+1] = bg; data[i+14] = bg; data[i+17] = bg; data[i+30] = bg; data[i+33] = bg
+			data[i+2] = bg; data[i+13] = bg; data[i+18] = bg; data[i+29] = bg; data[i+34] = bg
+			data[i+3] = bg; data[i+12] = bg; data[i+19] = bg; data[i+28] = bg; data[i+35] = bg
+			data[i+4] = bg; data[i+11] = bg; data[i+20] = bg; data[i+27] = bg; data[i+36] = bg
+			data[i+5] = bg; data[i+10] = bg; data[i+21] = bg; data[i+26] = bg; data[i+37] = bg
+			data[i+6] = bg; data[i+9]  = bg; data[i+22] = bg; data[i+25] = bg; data[i+38] = bg
+			data[i+7] = bg; data[i+8]  = bg; data[i+23] = bg; data[i+24] = bg; data[i+39] = bg
+	}
+}
--- ticker2.go
+++ ticker2.go
@@ -1,128 +1,20 @@
 package main
 
-import "fmt"
-import "time"
-import "strings"
-import "strconv"
-import ws2811 "github.com/rpi-ws281x/rpi-ws281x-go"
-
-const(
-	first= 5
-	brig = 255
-	poli = 8
-	pole = (8*5)
-	ledc = (pole*poli)+first // ((8*5 zhora dolu) * 5 poli) + 5 na zacatku
+import (
+		"fmt"
+		"time"
+		ws2811 "github.com/rpi-ws281x/rpi-ws281x-go"
 )
 
-func CharToRGB(fg uint32, bg uint32, c byte, i int, data []uint32){
-	switch c{
-		case '1':
-			data[i+0] = bg; data[i+15] = bg; data[i+16] = fg; data[i+31] = bg; data[i+32] = bg
-			data[i+1] = bg; data[i+14] = fg; data[i+17] = fg; data[i+30] = bg; data[i+33] = bg
-			data[i+2] = bg; data[i+13] = bg; data[i+18] = fg; data[i+29] = bg; data[i+34] = bg
-			data[i+3] = bg; data[i+12] = bg; data[i+19] = fg; data[i+28] = bg; data[i+35] = bg
-			data[i+4] = bg; data[i+11] = bg; data[i+20] = fg; data[i+27] = bg; data[i+36] = bg
-			data[i+5] = bg; data[i+10] = bg; data[i+21] = fg; data[i+26] = bg; data[i+37] = bg
-			data[i+6] = bg; data[i+9]  = bg; data[i+22] = fg; data[i+25] = bg; data[i+38] = bg
-			data[i+7] = bg; data[i+8]  = fg; data[i+23] = fg; data[i+24] = fg; data[i+39] = bg
-		case '2':
-			data[i+0] = bg; data[i+15] = fg; data[i+16] = fg; data[i+31] = fg; data[i+32] = bg
-			data[i+1] = fg; data[i+14] = bg; data[i+17] = bg; data[i+30] = bg; data[i+33] = fg
-			data[i+2] = bg; data[i+13] = bg; data[i+18] = bg; data[i+29] = bg; data[i+34] = fg
-			data[i+3] = bg; data[i+12] = bg; data[i+19] = bg; data[i+28] = bg; data[i+35] = fg
-			data[i+4] = bg; data[i+11] = bg; data[i+20] = bg; data[i+27] = fg; data[i+36] = bg
-			data[i+5] = bg; data[i+10] = bg; data[i+21] = fg; data[i+26] = bg; data[i+37] = bg
-			data[i+6] = bg; data[i+9]  = fg; data[i+22] = bg; data[i+25] = bg; data[i+38] = bg
-			data[i+7] = fg; data[i+8]  = fg; data[i+23] = fg; data[i+24] = fg; data[i+39] = fg
-		case '3':
-			data[i+0] = fg; data[i+15] = fg; data[i+16] = fg; data[i+31] = fg; data[i+32] = fg
-			data[i+1] = bg; data[i+14] = bg; data[i+17] = bg; data[i+30] = bg; data[i+33] = fg
-			data[i+2] = bg; data[i+13] = bg; data[i+18] = bg; data[i+29] = fg; data[i+34] = bg
-			data[i+3] = bg; data[i+12] = bg; data[i+19] = fg; data[i+28] = bg; data[i+35] = bg
-			data[i+4] = bg; data[i+11] = bg; data[i+20] = bg; data[i+27] = fg; data[i+36] = bg
-			data[i+5] = bg; data[i+10] = bg; data[i+21] = bg; data[i+26] = bg; data[i+37] = fg
-			data[i+6] = fg; data[i+9]  = bg; data[i+22] = bg; data[i+25] = bg; data[i+38] = fg
-			data[i+7] = bg; data[i+8]  = fg; data[i+23] = fg; data[i+24] = fg; data[i+39] = bg
-		case '4':
-			data[i+0] = fg; data[i+15] = bg; data[i+16] = bg; data[i+31] = bg; data[i+32] = bg
-			data[i+1] = fg; data[i+14] = bg; data[i+17] = bg; data[i+30] = bg; data[i+33] = bg
-			data[i+2] = fg; data[i+13] = bg; data[i+18] = fg; data[i+29] = bg; data[i+34] = bg
-			data[i+3] = fg; data[i+12] = bg; data[i+19] = fg; data[i+28] = bg; data[i+35] = bg
-			data[i+4] = fg; data[i+11] = bg; data[i+20] = fg; data[i+27] = bg; data[i+36] = bg
-			data[i+5] = fg; data[i+10] = fg; data[i+21] = fg; data[i+26] = fg; data[i+37] = fg
-			data[i+6] = bg; data[i+9]  = bg; data[i+22] = fg; data[i+25] = bg; data[i+38] = bg
-			data[i+7] = bg; data[i+8]  = bg; data[i+23] = fg; data[i+24] = bg; data[i+39] = bg
-		case '5':
-			data[i+0] = fg; data[i+15] = fg; data[i+16] = fg; data[i+31] = fg; data[i+32] = fg
-			data[i+1] = fg; data[i+14] = bg; data[i+17] = bg; data[i+30] = bg; data[i+33] = bg
-			data[i+2] = fg; data[i+13] = bg; data[i+18] = bg; data[i+29] = bg; data[i+34] = bg
-			data[i+3] = fg; data[i+12] = fg; data[i+19] = fg; data[i+28] = fg; data[i+35] = bg
-			data[i+4] = bg; data[i+11] = bg; data[i+20] = bg; data[i+27] = bg; data[i+36] = fg
-			data[i+5] = bg; data[i+10] = bg; data[i+21] = bg; data[i+26] = bg; data[i+37] = fg
-			data[i+6] = fg; data[i+9]  = bg; data[i+22] = bg; data[i+25] = bg; data[i+38] = fg
-			data[i+7] = bg; data[i+8]  = fg; data[i+23] = fg; data[i+24] = fg; data[i+39] = bg
-		case '6':
-			data[i+0] = bg; data[i+15] = fg; data[i+16] = fg; data[i+31] = fg; data[i+32] = bg
-			data[i+1] = fg; data[i+14] = bg; data[i+17] = bg; data[i+30] = bg; data[i+33] = fg
-			data[i+2] = fg; data[i+13] = bg; data[i+18] = bg; data[i+29] = bg; data[i+34] = bg
-			data[i+3] = fg; data[i+12] = fg; data[i+19] = fg; data[i+28] = fg; data[i+35] = bg
-			data[i+4] = fg; data[i+11] = bg; data[i+20] = bg; data[i+27] = bg; data[i+36] = fg
-			data[i+5] = fg; data[i+10] = bg; data[i+21] = bg; data[i+26] = bg; data[i+37] = fg
-			data[i+6] = fg; data[i+9]  = bg; data[i+22] = bg; data[i+25] = bg; data[i+38] = fg
-			data[i+7] = bg; data[i+8]  = fg; data[i+23] = fg; data[i+24] = fg; data[i+39] = bg
-		case '7':
-			data[i+0] = fg; data[i+15] = fg; data[i+16] = fg; data[i+31] = fg; data[i+32] = fg
-			data[i+1] = bg; data[i+14] = bg; data[i+17] = bg; data[i+30] = bg; data[i+33] = fg
-			data[i+2] = bg; data[i+13] = bg; data[i+18] = bg; data[i+29] = fg; data[i+34] = bg
-			data[i+3] = bg; data[i+12] = bg; data[i+19] = fg; data[i+28] = bg; data[i+35] = bg
-			data[i+4] = bg; data[i+11] = bg; data[i+20] = fg; data[i+27] = bg; data[i+36] = bg
-			data[i+5] = bg; data[i+10] = fg; data[i+21] = bg; data[i+26] = bg; data[i+37] = bg
-			data[i+6] = bg; data[i+9]  = fg; data[i+22] = bg; data[i+25] = bg; data[i+38] = bg
-			data[i+7] = bg; data[i+8]  = fg; data[i+23] = bg; data[i+24] = bg; data[i+39] = bg
-		case '8':
-			data[i+0] = bg; data[i+15] = fg; data[i+16] = fg; data[i+31] = fg; data[i+32] = bg
-			data[i+1] = fg; data[i+14] = bg; data[i+17] = bg; data[i+30] = bg; data[i+33] = fg
-			data[i+2] = fg; data[i+13] = bg; data[i+18] = bg; data[i+29] = bg; data[i+34] = fg
-			data[i+3] = bg; data[i+12] = fg; data[i+19] = fg; data[i+28] = fg; data[i+35] = bg
-			data[i+4] = fg; data[i+11] = bg; data[i+20] = bg; data[i+27] = bg; data[i+36] = fg
-			data[i+5] = fg; data[i+10] = bg; data[i+21] = bg; data[i+26] = bg; data[i+37] = fg
-			data[i+6] = fg; data[i+9]  = bg; data[i+22] = bg; data[i+25] = bg; data[i+38] = fg
-			data[i+7] = bg; data[i+8]  = fg; data[i+23] = fg; data[i+24] = fg; data[i+39] = bg
-		case '9':
-			data[i+0] = bg; data[i+15] = fg; data[i+16] = fg; data[i+31] = fg; data[i+32] = bg
-			data[i+1] = fg; data[i+14] = bg; data[i+17] = bg; data[i+30] = bg; data[i+33] = fg
-			data[i+2] = fg; data[i+13] = bg; data[i+18] = bg; data[i+29] = bg; data[i+34] = fg
-			data[i+3] = fg; data[i+12] = bg; data[i+19] = bg; data[i+28] = bg; data[i+35] = fg
-			data[i+4] = bg; data[i+11] = fg; data[i+20] = fg; data[i+27] = fg; data[i+36] = fg
-			data[i+5] = bg; data[i+10] = bg; data[i+21] = bg; data[i+26] = bg; data[i+37] = fg
-			data[i+6] = fg; data[i+9]  = bg; data[i+22] = bg; data[i+25] = bg; data[i+38] = fg
-			data[i+7] = bg; data[i+8]  = fg; data[i+23] = fg; data[i+24] = fg; data[i+39] = bg
-		case '0':
-			data[i+0] = bg; data[i+15] = fg; data[i+16] = fg; data[i+31] = fg; data[i+32] = bg
-			data[i+1] = fg; data[i+14] = bg; data[i+17] = bg; data[i+30] = bg; data[i+33] = fg
-			data[i+2] = fg; data[i+13] = bg; data[i+18] = bg; data[i+29] = bg; data[i+34] = fg
-			data[i+3] = fg; data[i+12] = bg; data[i+19] = bg; data[i+28] = fg; data[i+35] = fg
-			data[i+4] = fg; data[i+11] = bg; data[i+20] = fg; data[i+27] = bg; data[i+36] = fg
-			data[i+5] = fg; data[i+10] = fg; data[i+21] = bg; data[i+26] = bg; data[i+37] = fg
-			data[i+6] = fg; data[i+9]  = bg; data[i+22] = bg; data[i+25] = bg; data[i+38] = fg
-			data[i+7] = bg; data[i+8]  = fg; data[i+23] = fg; data[i+24] = fg; data[i+39] = bg
-		default :
-			data[i+0] = bg; data[i+15] = bg; data[i+16] = bg; data[i+31] = bg; data[i+32] = bg
-			data[i+1] = bg; data[i+14] = bg; data[i+17] = bg; data[i+30] = bg; data[i+33] = bg
-			data[i+2] = bg; data[i+13] = bg; data[i+18] = bg; data[i+29] = bg; data[i+34] = bg
-			data[i+3] = bg; data[i+12] = bg; data[i+19] = bg; data[i+28] = bg; data[i+35] = bg
-			data[i+4] = bg; data[i+11] = bg; data[i+20] = bg; data[i+27] = bg; data[i+36] = bg
-			data[i+5] = bg; data[i+10] = bg; data[i+21] = bg; data[i+26] = bg; data[i+37] = bg
-			data[i+6] = bg; data[i+9]  = bg; data[i+22] = bg; data[i+25] = bg; data[i+38] = bg
-			data[i+7] = bg; data[i+8]  = bg; data[i+23] = bg; data[i+24] = bg; data[i+39] = bg
-	}
-}
-
-func TextToRGB(fg uint32, bg uint32, text string, data []uint32){
-	for i := 0; i < len(text) && i < poli; i++ {
-		CharToRGB(fg, bg, text[i], (i*pole)+first, data)
-	}
-}
+const (
+	first= 5 // first 5 leds before panels 
+	ti = 5 // time interval in seconds
+	ilt = 0 // illumination threshold
+	brig = 255
+	panels = 8
+	panel = (8*5)
+	ledc = (panels*panel)+first // ((8*5 from top to bottom) * 5 panels) + leds from start 
+)
 
 func main(){
 	opt := ws2811.DefaultOptions
@@ -132,16 +24,39 @@ func main(){
 
 	dev, _ := ws2811.MakeWS2811(&opt)
 
+	lt := time.Now()
+	format := "0"
+	stav := 0
+	var fg, bg uint32
+
 	dev.Init()
 	defer dev.Fini()
 	for {
+		t := time.Now()
+		if t.After(lt.Add(ti * time.Second)) {
+			il := GetIll()
+			if stav == 0 {
+				format = fmt.Sprintf("%8d", GetPrice())
+				fg, bg = 0xffff00, 0x000000
+				if il <= ilt { fg, bg = 0x706000, 0x000000 }
+				stav = 1
+			} else if stav == 1 {
+				format = fmt.Sprintf("%8.1f", GetTemp())
+				fg, bg = 0xff0040, 0x000000
+				if il <= ilt { fg, bg = 0x600010, 0x000000 }
+				stav = 2
+			} else {
+				format = fmt.Sprintf("%8.1f", GetPress()/100)
+				fg, bg = 0x00ff00, 0x000000
+				if il <= ilt { fg, bg = 0x006000, 0x000000 }
+				stav = 0
+			}
+			fmt.Println(format)
+			lt = t
+		}
 
-		btc := strings.Split(GetPrice(),".")
-		btci, _ := strconv.Atoi(btc[0])
-		format := fmt.Sprintf("%8d", btci)
-	//	fmt.Println(format)
-		TextToRGB(0x00ff00, 0x000000, format, dev.Leds(0))
+		TextToRGB(fg, bg, format, dev.Leds(0))
 		dev.Render()
-		time.Sleep(10000 * time.Millisecond)
+		time.Sleep(10 * time.Millisecond)
 	}
 }
